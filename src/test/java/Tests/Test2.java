d464f70a1cb13618aef910b2b8fc9fdb232c9558
sethu416 <118735803+sethu416@users.noreply.github.com>
2022-11-22 10:53:08 +0530

    ram has changed thhis

--- src/test/java/Tests/Test2.java
+++ src/test/java/Tests/Test2.java
@@ -10,6 +10,7 @@ import Resources.Base;
 public class Test2 extends Base {
 	@Test
 	public void test2() throws IOException, InterruptedException {
+		System.out.println("ram has updated this page");
 		System.out.println("testtwo");
 		WebDriver driver=intializeDriver();
 		driver.get("http://tutorialsninja.com/demo/index.php?route=common/home");
